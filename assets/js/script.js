60f1d5d482d1948f3593f66bcdda337234c55b3e
Catherine  Hull <cathh127@hotmail.com>
2024-04-06 17:06:47 +0000

    end of day commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
 
-const question = 
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("btn-next");
+
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestions();
+}
+
+function showQuestions() {
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo =  currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn-answer");
+        answerButton.appendChild(button);
+    });
 }
