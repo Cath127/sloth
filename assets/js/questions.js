bbe03ae33f02774c86bb301762fcc79183462c9e
Catherine  Hull <cathh127@hotmail.com>
2024-04-08 10:31:25 +0000

    Add questions to script.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,108 +1,110 @@
-const questions = [
-    {
-        question: "What is the average lifespan of sloths in the wild?",
-    
-        answers: [
-          { text: "15-20 years", correct: false },
-          { text: "20-25 years", correct: correct },
-          { text: "30-35 years", correct: false },
-          { text: "40-45 years", correct: false },
-        ],
-    },
-    {
-        question: "How many hours does it take for a sloth to digest a meal",
-    
-        answers: [
-          { text: "6-8 hours", correct: false },
-          { text: "12-24 hours", correct: false },
-          { text: "36-48 hours", correct: correct },
-          { text: "72-96 hours", correct: false },
-        ],
-    },
-    {
-        question: "Which of the following is true about sloths fur?",
-    
-        answers: [
-          { text: "It is naturally blue in colour", correct: false },
-          { text: "It grows in the opposite direction to other mammals", correct: correct },
-          { text: "It sheds once a year", correct: false },
-          { text: "It is resistant to parasites", correct: false },
-        ],
-    },
 
-    {
-        question: "What is the weight of a newborn sloth?",
-    
-        answers: [
-          { text: "1-2 pounds", correct: correct },
-          { text: "3-5 pounds", correct: false },
-          { text: "6-8 pounds", correct: false },
-          { text: "9-11 pounds", correct: false },
-        ],
-    },
 
-    {
-        question: "Who is a natural predator of sloths?",
+// const questions = [
+//     {
+//         question: "What is the average lifespan of sloths in the wild?",
     
-        answers: [
-          { text: "jaguars", correct: correct },
-          { text: "elephants", correct: false },
-          { text: "crocodiles", correct: false },
-          { text: "gorillas", correct: false },
-        ],
-    },
+//         answers: [
+//           { text: "15-20 years", correct: false },
+//           { text: "20-25 years", correct: correct },
+//           { text: "30-35 years", correct: false },
+//           { text: "40-45 years", correct: false },
+//         ],
+//     },
+//     {
+//         question: "How many hours does it take for a sloth to digest a meal",
+    
+//         answers: [
+//           { text: "6-8 hours", correct: false },
+//           { text: "12-24 hours", correct: false },
+//           { text: "36-48 hours", correct: correct },
+//           { text: "72-96 hours", correct: false },
+//         ],
+//     },
+//     {
+//         question: "Which of the following is true about sloths fur?",
+    
+//         answers: [
+//           { text: "It is naturally blue in colour", correct: false },
+//           { text: "It grows in the opposite direction to other mammals", correct: correct },
+//           { text: "It sheds once a year", correct: false },
+//           { text: "It is resistant to parasites", correct: false },
+//         ],
+//     },
 
-    {
-        question: "Where are sloths found in the wild?",
+//     {
+//         question: "What is the weight of a newborn sloth?",
     
-        answers: [
-          { text: "Africa", correct: false },
-          { text: "Asia", correct: false },
-          { text: "Australia", correct: false },
-          { text: "Central and South America", correct: correct },
-        ],
-    },
+//         answers: [
+//           { text: "1-2 pounds", correct: correct },
+//           { text: "3-5 pounds", correct: false },
+//           { text: "6-8 pounds", correct: false },
+//           { text: "9-11 pounds", correct: false },
+//         ],
+//     },
 
-    {
-        question: "How many hours a day do sloths sleep?",
+//     {
+//         question: "Who is a natural predator of sloths?",
     
-        answers: [
-          { text: "8-10 hours", correct: false },
-          { text: "10-12 hours", correct: false },
-          { text: "12-14 hours", correct: correct },
-          { text: "14-16 hours", correct: false },
-        ],
-    },
+//         answers: [
+//           { text: "jaguars", correct: correct },
+//           { text: "elephants", correct: false },
+//           { text: "crocodiles", correct: false },
+//           { text: "gorillas", correct: false },
+//         ],
+//     },
 
-    {
-        question: "How many chambers does a sloth stomach have?",
+//     {
+//         question: "Where are sloths found in the wild?",
     
-        answers: [
-          { text: "one", correct: false },
-          { text: "two", correct: correct },
-          { text: "three", correct: false },
-          { text: "four", correct: false },
-        ],
-    },
+//         answers: [
+//           { text: "Africa", correct: false },
+//           { text: "Asia", correct: false },
+//           { text: "Australia", correct: false },
+//           { text: "Central and South America", correct: correct },
+//         ],
+//     },
 
-    {
-        question: "Which sense is the best for sloths?",
+//     {
+//         question: "How many hours a day do sloths sleep?",
     
-        answers: [
-          { text: "sight", correct: false },
-          { text: "taste", correct: false },
-          { text: "touch", correct: false },
-          { text: "smell", correct: correct },
-        ],
-    },
+//         answers: [
+//           { text: "8-10 hours", correct: false },
+//           { text: "10-12 hours", correct: false },
+//           { text: "12-14 hours", correct: correct },
+//           { text: "14-16 hours", correct: false },
+//         ],
+//     },
 
-    {
-        question: "What is the main diet of a sloth?",
+//     {
+//         question: "How many chambers does a sloth stomach have?",
     
-        answers: [
-          { text: "insects", correct: false },
-          { text: "meat", correct: false },
-          { text: "leaves", correct: correct },
-          { text: "fish", correct: false },
-        ],
-    }
+//         answers: [
+//           { text: "one", correct: false },
+//           { text: "two", correct: correct },
+//           { text: "three", correct: false },
+//           { text: "four", correct: false },
+//         ],
+//     },
+
+//     {
+//         question: "Which sense is the best for sloths?",
+    
+//         answers: [
+//           { text: "sight", correct: false },
+//           { text: "taste", correct: false },
+//           { text: "touch", correct: false },
+//           { text: "smell", correct: correct },
+//         ],
+//     },
+
+//     {
+//         question: "What is the main diet of a sloth?",
+    
+//         answers: [
+//           { text: "insects", correct: false },
+//           { text: "meat", correct: false },
+//           { text: "leaves", correct: correct },
+//           { text: "fish", correct: false },
+//         ],
+//     }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,106 @@
 
-const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("btn-next");
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
 
+let currentQuestion = {};
+let acceptingAnswer = true;  //To create a delay - Do I need this?//
+let score = 0;  //Always starts at zero//
+let questionCounter = 0; //What questions you are on//
+let availableQuestions = [];  //Copy of full question set and take questions out as we use them to always find a unique question - DO I need this?//
 
-let currentQuestionIndex = 0;
-let score = 0;
+let questions = [
+    {
+        question: "What is the average lifespan of sloths in the wild?",
+        choice1: "15-20 years",
+        choice2: "20-25 years",
+        choice3: "30-35 years",
+        choice4: "40-45 years",
+        answer: 2,
+    },
+    {
+        question: "How many hours does it take for a sloth to digest a meal?",
+        choice1: "6-8 hours",
+        choice2: "12-24 hours",
+        choice3: "36-48 hours",
+        choice4: "72-96 hours",
+        answer: 3,
+    },
+    {
+        question: "Which is true about sloths fur?",
+        choice1: "It's naturally blue in colour",
+        choice2: "It grows the opposite direction to other mammals",
+        choice3: "It sheds once a year",
+        choice4: "It is resisant to parasites",
+        answer: 2,
+    },
+    {
+        question: "How heavy is a newborn sloth?",
+        choice1: "1-2 pounds",
+        choice2: "3-5 pounds",
+        choice3: "6-8 pounds",
+        choice4: "9-11 pounds",
+        answer: 1,
+    },
+    {
+        question: "Who is a natural sloth predator?",
+        choice1: "elephants",
+        choice2: "crocodiles",
+        choice3: "gorillas",
+        choice4: "jaguars",
+        answer: 4,
+    },
+    {
+        question: "Where are sloths found in the wild?",
+        choice1: "Africa",
+        choice2: "Asia",
+        choice3: "Australia",
+        choice4: "Central and South America",
+        answer: 4,
+    },
+    {
+        question: "How many hours a day do sloths sleep?",
+        choice1: "8-10 hours",
+        choice2: "10-12 hours",
+        choice3: "12-14 hours",
+        choice4: "14-16 hours",
+        answer: 3,
+    },
+    {
+        question: "How many chambers does a sloth stomach have?",
+        choice1: "1",
+        choice2: "2",
+        choice3: "3",
+        choice4: "4",
+        answer: 2,
+    },
+    {
+        question: "Which sense is the best for sloths?",
+        choice1: "sight",
+        choice2: "taste",
+        choice3: "touch",
+        choice4: "smell",
+        answer: 4,
+    },
+    {
+        question: "What is the main diet of sloths?",
+        choice1: "insects",
+        choice2: "meat",
+        choice3: "fish",
+        choice4: "leaves",
+        answer: 4,
+    },
+];
 
-function startQuiz() {
-    currentQuestionIndex = 0;
+//CONSTANTS//
+const CORRECT_BONUS = 10;  //How much a right question is worth//
+const MAX_QUESTIONS = 3; // How many questions does a user get - Dont need this// 
+
+//START GAME
+startGame() => {
+    questioncounter = 0; //Make sure the counter is starting at zero - probably is so may not need this//
     score = 0;
-    nextButton.innerHTML = "Next";
-    showQuestions();
+    availableQuestions = [...questions];  //To get questions - don't need this
+    console.log(availableQuestions);
 }
 
-function showQuestions() {
-    let currentQuestion = questions[currentQuestionIndex];
-    let questionNo =  currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
-
-    currentQuestion.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerHTML = answer.text;
-        button.classList.add("btn-answer");
-        answerButton.appendChild(button);
-    });
-}
+startGame();
