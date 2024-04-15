ea987ee2b2b8b586e3714c62a5ce34401515fa6a
Catherine  Hull <cathh127@hotmail.com>
2024-04-15 14:17:51 +0000

    Add end screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,156 +1,194 @@
-//Get DOM Elements
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName('choice-text')); // This converts the choices text from an Html collection to an array
-
-//Variables
-let currentQuestion = {};
-let acceptingAnswers = false;  //To create a delay - Do I need this?
-let score = 0;  //Always starts at zero
-let questionCounter = 0; //What questions you are on
-let availableQuestions = [];  //Copy of full question set and take questions out as we use them to always find a unique question - DO I need this?
-
-//Let used for questions so the order can be random
 let questions = [
     {
         question: "What is the average lifespan of sloths in the wild?",
-        choice1: "15-20 years",
-        choice2: "20-25 years",
-        choice3: "30-35 years",
-        choice4: "40-45 years",
-        answer: 2
+    
+        answers: [
+          { text: "15-20 years", correct: false },
+          { text: "20-25 years", correct: correct },
+          { text: "30-35 years", correct: false },
+          { text: "40-45 years", correct: false },
+        ],
     },
     {
-        question: "How many hours does it take for a sloth to digest a meal?",
-        choice1: "6-8 hours",
-        choice2: "12-24 hours",
-        choice3: "36-48 hours",
-        choice4: "72-96 hours",
-        answer: 3
+        question: "How many hours does it take for a sloth to digest a meal",
+    
+        answers: [
+          { text: "6-8 hours", correct: false },
+          { text: "12-24 hours", correct: false },
+          { text: "36-48 hours", correct: correct },
+          { text: "72-96 hours", correct: false },
+        ],
     },
     {
-        question: "How many hours can a sloth eat each day?",
-        choice1: "up to 2 hours",
-        choice2: "up to 4 hours",
-        choice3: "up to 6 hours",
-        choice4: "up to 8 hours",
-        answer: 3
+        question: "Which of the following is true about sloths fur?",
+    
+        answers: [
+          { text: "It is naturally blue in colour", correct: false },
+          { text: "It grows in the opposite direction to other mammals", correct: correct },
+          { text: "It sheds once a year", correct: false },
+          { text: "It is resistant to parasites", correct: false },
+        ],
     },
+
     {
-        question: "How heavy is a newborn sloth?",
-        choice1: "1-2 pounds",
-        choice2: "3-5 pounds",
-        choice3: "6-8 pounds",
-        choice4: "9-11 pounds",
-        answer: 1
+        question: "What is the weight of a newborn sloth?",
+    
+        answers: [
+          { text: "1-2 pounds", correct: correct },
+          { text: "3-5 pounds", correct: false },
+          { text: "6-8 pounds", correct: false },
+          { text: "9-11 pounds", correct: false },
+        ],
     },
+
     {
-        question: "Who is a natural sloth predator?",
-        choice1: "elephants",
-        choice2: "crocodiles",
-        choice3: "gorillas",
-        choice4: "jaguars",
-        answer: 4
+        question: "Who is a natural predator of sloths?",
+    
+        answers: [
+          { text: "jaguars", correct: correct },
+          { text: "elephants", correct: false },
+          { text: "crocodiles", correct: false },
+          { text: "gorillas", correct: false },
+        ],
     },
+
     {
         question: "Where are sloths found in the wild?",
-        choice1: "Africa",
-        choice2: "Asia",
-        choice3: "Australia",
-        choice4: "Central and South America",
-        answer: 4
+    
+        answers: [
+          { text: "Africa", correct: false },
+          { text: "Asia", correct: false },
+          { text: "Australia", correct: false },
+          { text: "Central and South America", correct: correct },
+        ],
     },
+
     {
         question: "How many hours a day do sloths sleep?",
-        choice1: "8-10 hours",
-        choice2: "10-12 hours",
-        choice3: "12-14 hours",
-        choice4: "14-16 hours",
-        answer: 3
+    
+        answers: [
+          { text: "8-10 hours", correct: false },
+          { text: "10-12 hours", correct: false },
+          { text: "12-14 hours", correct: correct },
+          { text: "14-16 hours", correct: false },
+        ],
     },
+
     {
         question: "How many chambers does a sloth stomach have?",
-        choice1: "1",
-        choice2: "2",
-        choice3: "3",
-        choice4: "4",
-        answer: 2
+    
+        answers: [
+          { text: "one", correct: false },
+          { text: "two", correct: correct },
+          { text: "three", correct: false },
+          { text: "four", correct: false },
+        ],
     },
+
     {
         question: "Which sense is the best for sloths?",
-        choice1: "smell",
-        choice2: "taste",
-        choice3: "touch",
-        choice4: "sight",
-        answer: 1
+    
+        answers: [
+          { text: "sight", correct: false },
+          { text: "taste", correct: false },
+          { text: "touch", correct: false },
+          { text: "smell", correct: correct },
+        ],
     },
     {
-        question: "What is the main diet of sloths?",
-        choice1: "insects",
-        choice2: "meat",
-        choice3: "fish",
-        choice4: "leaves",
-        answer: 4
-    },
-];
+        question: "What is the main diet of a sloth?",
+    
+        answers: [
+          { text: "insects", correct: false },
+          { text: "meat", correct: false },
+          { text: "leaves", correct: correct },
+          { text: "fish", correct: false },
+        ],
+    }
+]
 
-//CONSTANT
-const CORRECT_BONUS = 10;  //How much a right question is worth
-const MAX_QUESTIONS = 3; // How many questions does a user get - Dont need this
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
 
-//START GAME
-startGame = () => {
-    questionCounter = 0; //Make sure the counter is starting at zero - probably is so may not need this
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
     score = 0;
-    availableQuestions = [...questions];  //To get questions - don't need this
-    getNewQuestion();
-};
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
 
-//TO ADD 1 TO THE ANSWERS WHEN CORRECT
-getNewQuestion = () => {
-    questionCounter++;
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);  //To get a random question - may need to put it somewhere else
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
-    choices.forEach((choice) => {
-        const number = choice.dataset['number'];
-        choice.innerHTML = currentQuestion['choice' + number];  //To get the right answer using data number
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
     });
-
-    availableQuestions.splice(questionIndex, 1); //Gets rid of the question - may not need this
-    acceptingAnswers = true; //Allows user to answer
-}; 
+}
 
 
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
 
-choices.forEach((choice) => {
-    choice.addEventListener('click', (e) => {
-        if (!acceptingAnswers) return;  //If not ready for the player to click the answer
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
 
-        
-        acceptingAnswers = false;  //To make a delay
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
 
-       //TO SEE IF THE ANSWER IS CORRECT
-        const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
-
-        selectedChoice.parentElement.classList.add(classToApply);
-        console.log(classToApply);
-
-        // selectedChoice.parentElement.classList.remove(classToApply);
-
-        // setTimeout(() => {
-        //     selectedChoice.parentElement.classList.remove(classToApply);
-        //     getNewQuestion();
-        //   }, 1000);
-
-      
-        // console.log(selectedAnswer == currentQuestion.answer);
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
 
 
-        getNewQuestion();
-    }); 
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
 });
 
-startGame();
+
+startQuiz();
